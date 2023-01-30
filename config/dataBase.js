b50fa4e9ff3b35f4f1d787d11b91a898ea7040a5
nabi786 <rehanpardesi2018@gmail.com>
2023-01-30 16:32:41 +0500

    updated 4:32

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,16 +1,17 @@
 // require('dotenv').config()
-const mongoose = require('mongoose')
-
-
+const mongoose = require("mongoose");
 
 // mongoose.connect('mongodb://localhost:27017/buenotechapp',{
-    // console.log(process.env.dataBase)
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.dataBase,{
+// console.log(process.env.dataBase)
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.dataBase, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>{
-    console.log('mongoose connect successfully')
-}).catch((err)=>{
-    console.log(err)
-})
+  })
+  .then(() => {
+    console.log("mongoose connect successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- index.js
+++ index.js
@@ -1,78 +1,58 @@
-require('dotenv').config()
-const { MongoClient } = require('mongodb');
-require("./config/dataBase")
-const morgan = require("morgan")
-const express = require('express')
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+require("./config/dataBase");
+const morgan = require("morgan");
+const express = require("express");
 const app = express();
 const PORT = 5000 || process.env.PORT;
 const cors = require("cors");
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 // global Middlewares
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json())
 // working
 
-
 const uri = process.env.dataBase;
 const client = new MongoClient(uri);
 
+app.use(cors());
+app.use(morgan("dev"));
 
-
-app.use(cors())
-app.use(morgan('dev'))
-
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-  
+
 // app.use(cors(corsOpts));
 // oriring error
 
-
-
-
 // API Routes
-const indexRout = require('./routers/index')
-const LockToken = require('./routers/LockToken')
-const LockBNB = require('./routers/lockBNB')
-const serviceFee = require('./routers/servicesFees')
-const AdminLoing = require("./routers/adminLong")
-const wallets = require('./routers/wallets')
-
+const indexRout = require("./routers/index");
+const LockToken = require("./routers/LockToken");
+const LockBNB = require("./routers/lockBNB");
+const serviceFee = require("./routers/servicesFees");
+const AdminLoing = require("./routers/adminLong");
+const wallets = require("./routers/wallets");
 
 // using router as a global middleware
 app.use(indexRout);
-app.use("/api",LockToken);
-app.use("/api",LockBNB);
-app.use("/api",serviceFee);
-app.use("/api",AdminLoing);
-app.use("/api",wallets);
+app.use("/api", LockToken);
+app.use("/api", LockBNB);
+app.use("/api", serviceFee);
+app.use("/api", AdminLoing);
+app.use("/api", wallets);
 
-
-
-
-
-
-
-
-
-// if router not found 
-app.get("*", (req,res)=>{
-    res.status(404).json({msg : "page not found"})
+// if router not found
+app.get("*", (req, res) => {
+  res.status(404).json({ msg: "page not found" });
 });
 
-
-
-
-
-
-
-client.connect(err => {
-
-    if(err){ console.error(err); return false;}
-    app.listen(PORT, () => {
-            console.log(`server started successfully on PORT Number ${PORT}`)
-        })
-
+client.connect((err) => {
+  if (err) {
+    console.error(err);
+    return false;
+  }
+  app.listen(PORT, () => {
+    console.log(`server started successfully on PORT Number ${PORT}`);
+  });
 });
